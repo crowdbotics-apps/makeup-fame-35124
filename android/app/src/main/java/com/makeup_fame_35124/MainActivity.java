1117c72b474ec2cd219dcebfd9fec89ae03094d5
Crowdbotics <team@crowdbotics.com>
2022-05-07 19:38:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/makeup_fame_35124/MainActivity.java
@@ -0,0 +1,15 @@
+package com.makeup_fame_35124;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "makeup_fame_35124";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "makeup_fame_35124",
+  url: "https://makeup_fame_35124.botics.co",
+  api: "https://makeup_fame_35124.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
